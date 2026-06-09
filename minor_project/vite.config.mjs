364e948f37efb31d92874bc758f3dbce38833167
prerakbisen <prerakbisen@gmail.com>
2026-06-09 22:12:38 +0530

    bug fixed

--- minor_project/vite.config.mjs
+++ minor_project/vite.config.mjs
@@ -7,13 +7,13 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: "build",
+    outDir: "dist",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
     port: 4028,
     host: "0.0.0.0",
-    strictPort: true
+    strictPort: false,
   }
 });
